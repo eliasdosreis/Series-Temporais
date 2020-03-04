4eaa74386378082f7fd70ebe700f371e0a25c308
Elias <elias_dos_reis@hotmail.com>
2020-03-04 07:57:21 -0300

    Primeiro Comentario

---
+++ Criando_Serie_Temporal.R
@@ -0,0 +1,10 @@
+install.packages('forecast')
+install.packages('ggplot2')
+install.packages('urca')
+install.packages('latest')
+install.packages('seasonal')
+install.packages('seasonalview')
+
+myts =rnorm(60)
+myts = ts(myts, start = c(2012,1),c(2016,12), frequency = 12)
+myts
